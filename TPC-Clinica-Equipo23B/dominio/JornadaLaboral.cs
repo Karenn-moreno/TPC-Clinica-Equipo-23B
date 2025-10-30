24ceee9d10cbc31e19c6055cedee9d49c555ca33
Victoria <victoria.caseres2005@gmail.com>
2025-10-30 15:34:04 -0300

    Creación de clase permiso, persona, jornadalaboral y la logica de accso a datos

---
+++ TPC-Clinica-Equipo23B/dominio/JornadaLaboral.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class JornadaLaboral
+    {
+        public int IdJornadaLaboral { get; set; }
+        public TimeSpan HorarioInicio { get; set; }
+        public TimeSpan HoraFin { get; set; }
+        public DiaLaboral DiaLaboral { get; set; }
+    }
+
+    public enum DiaLaboral
+    {
+        Lunes,
+        Martes,
+        Miercoles,
+        Jueves,
+        Viernes,
+        Sabado,
+        Domingo
+    }
+
+
+}
---
+++ TPC-Clinica-Equipo23B/dominio/RolPermiso.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class RolPermiso
+    {
+        public int IdRol { get; set; }
+        public Rol Rol { get; set; }
+        public int IdPermiso { get; set; }
+        public Permiso Permiso { get; set; }
+
+    }
+}
