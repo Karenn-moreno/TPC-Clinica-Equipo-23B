d5139a41975fd8424d09d51bd1bec3232815510d
Anahi <anahimilenagimenez@gmail.com>
2025-11-11 14:40:42 -0300

    Eliminacion de genero en registro

--- TPC-Clinica-Equipo23B/ClinicaWeb/Registrarse.aspx.designer.cs
+++ TPC-Clinica-Equipo23B/ClinicaWeb/Registrarse.aspx.designer.cs
@@ -60,15 +60,6 @@ namespace ClinicaWeb
         protected global::System.Web.UI.WebControls.TextBox idDocument;
 
         /// <summary>
-        /// Control gender.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList gender;
-
-        /// <summary>
         /// Control email.
         /// </summary>
         /// <remarks>
